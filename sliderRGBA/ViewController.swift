1ee272ea088434a77a971b0289a89300a30f78e9
Nicholas <nicholas11858@gmail.com>
2021-05-23 16:39:55 +0300

    Initial Commit

---
+++ sliderRGBA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sliderRGBA
+//
+//  Created by NIKOLAY OSIPOV on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
